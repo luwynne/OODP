51c97de6f9c0b2d3a355960ce446def811327c29
Luiz Wynne <luwynne@aol.co.uk>
2018-05-02 17:01:46 +0100

    New singleton

--- MenuTrader.java
+++ MenuTrader.java
@@ -1,3 +1,63 @@
+//package commercialtradesystem.OODP;
+//
+//
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+///**
+// * System menu class
+// * It contains a runnable main method
+// * @author thiago
+// * @version 1.0.0
+// */
+//public class MenuTrader {
+//
+//
+//    public static void main(String args[]) {
+//
+//        Singleton sing = Singleton.getInstance();
+//        
+//        reportByCompany reportComp = new reportByCompany();
+//        overallReportOfCompanies reportCompanies = new overallReportOfCompanies(); 
+//        CommercialTradingSystemFacade myFacade = new CommercialTradingSystemFacade(reportComp,reportCompanies);
+//        
+//
+//        System.out.println("|--------------------------------------------------------------|");
+//        System.out.println("|----------- WELCOME TO COMMERCIAL TRADDING SYSTEM ------------|");
+//        System.out.println("|--------------------------------------------------------------|");
+//        System.out.println( sing.menu );
+//        System.out.println("|--------------------------------------------------------------|");
+//        Scanner scanner = new Scanner(System.in);
+//        String opt = scanner.nextLine();
+//        opt = opt.toLowerCase();
+//
+//        
+//
+//        if(null != opt)switch (opt) {
+//            case "a":
+//                System.out.println("|-- Trading for company BigA");
+//                myFacade.SelectCompany("a");
+//                break;
+//            case "b":
+//                System.out.println("|-- Trading for company BigB");
+//                myFacade.SelectCompany("b");
+//                break;
+//            case "c":
+//                System.out.println("|-- Trading for company BigC");
+//                myFacade.SelectCompany("c");
+//                break;
+//            case "z":
+//                myFacade.AutoTrader();
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        System.out.println("|--------------------------------------------------------------|");
+//
+//    }
+//}
+
 package commercialtradesystem.OODP;
 
 
@@ -7,53 +67,34 @@ import java.util.Scanner;
 /**
  * System menu class
  * It contains a runnable main method
- * @author thiago
- * @version 1.0.0
  */
 public class MenuTrader {
 
 
-    public static void main(String args[]) {
-
-        Singleton sing = Singleton.getInstance();
-        
-        reportByCompany reportComp = new reportByCompany();
-        overallReportOfCompanies reportCompanies = new overallReportOfCompanies(); 
-        CommercialTradingSystemFacade myFacade = new CommercialTradingSystemFacade(reportComp,reportCompanies);
-        
+    public static void main(String args[]) throws IOException{
 
         System.out.println("|--------------------------------------------------------------|");
         System.out.println("|----------- WELCOME TO COMMERCIAL TRADDING SYSTEM ------------|");
         System.out.println("|--------------------------------------------------------------|");
-        System.out.println( sing.menu );
+        System.out.println("|- Trade with the company BigA, BigB, BigC (Type: A / B / C )\n|- Trade autonomously (Type: Z)");
         System.out.println("|--------------------------------------------------------------|");
         Scanner scanner = new Scanner(System.in);
         String opt = scanner.nextLine();
         opt = opt.toLowerCase();
 
-        
 
-        if(null != opt)switch (opt) {
-            case "a":
-                System.out.println("|-- Trading for company BigA");
-                myFacade.SelectCompany("a");
-                break;
-            case "b":
-                System.out.println("|-- Trading for company BigB");
-                myFacade.SelectCompany("b");
-                break;
-            case "c":
-                System.out.println("|-- Trading for company BigC");
-                myFacade.SelectCompany("c");
-                break;
-            case "z":
-                myFacade.AutoTrader();
-                break;
-            default:
-                break;
+        Singleton singleton = new Singleton();
+
+
+
+        if(null != opt){
+
+            singleton.startFacade(opt);
+
         }
 
         System.out.println("|--------------------------------------------------------------|");
 
     }
 }
+
--- Singleton.java
+++ Singleton.java
@@ -1,24 +1,72 @@
+//package commercialtradesystem.OODP;
+//
+//
+///**
+// * Singleton class responsible for single instantiation of Menu
+// * @author lwynne
+// * @version 1.0.0
+// */
+//class Singleton {
+//
+//    private static Singleton single_instance = null; // static variable single_instance of type Singleton
+//    public String menu; //variable of type String
+//
+//    private Singleton(){
+//        menu = "|- Trade with the company BigA, BigB, BigC (Type: A / B / C )\n|- Trade autonomously (Type: Z)";
+//    }
+//
+//    public static Singleton getInstance(){
+//        if(single_instance == null){
+//            single_instance = new Singleton();
+//        }
+//        return single_instance;
+//    }
+//}
+
+
 package commercialtradesystem.OODP;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
  * Singleton class responsible for single instantiation of Menu
- * @author lwynne
- * @version 1.0.0
+ *
  */
 class Singleton {
 
-    private static Singleton single_instance = null; // static variable single_instance of type Singleton
-    public String menu; //variable of type String
 
-    private Singleton(){
-        menu = "|- Trade with the company BigA, BigB, BigC (Type: A / B / C )\n|- Trade autonomously (Type: Z)";
-    }
+    CommercialTradingSystemFacade myFacade;
 
-    public static Singleton getInstance(){
-        if(single_instance == null){
-            single_instance = new Singleton();
+    public void startFacade(String option) throws IOException{
+
+        if(myFacade == null){
+
+            switch (option){
+                case "a":
+                    myFacade = new CommercialTradingSystemFacade();
+                    myFacade.SelectCompany("a");
+                    break;
+                case "b":
+                    myFacade = new CommercialTradingSystemFacade();
+                    myFacade.SelectCompany("b");
+                    break;
+                case "c":
+                    myFacade = new CommercialTradingSystemFacade();
+                    myFacade.SelectCompany("c");
+                    break;
+                case "z":
+                        myFacade = new CommercialTradingSystemFacade();
+                        myFacade.AutoTrader();
+                    break;
+            }
+
         }
-        return single_instance;
+
     }
+
+
+
 }
